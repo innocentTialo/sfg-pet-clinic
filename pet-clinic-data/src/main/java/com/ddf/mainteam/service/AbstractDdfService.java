6578ed669929988b010379f597bbf60fe9531d6d
InnocentTIALO <tialoinnocent@gmail.com>
2020-05-23 17:58:23 +0100

    Refactoring AbstractDdfService. closes #10

--- pet-clinic-data/src/main/java/com/ddf/mainteam/service/AbstractDdfService.java
+++ pet-clinic-data/src/main/java/com/ddf/mainteam/service/AbstractDdfService.java
@@ -5,13 +5,13 @@ import java.util.List;
 /**
  * Created by InnocentTIALO on 5/23/2020.
  */
-public abstract class AbstractDdfService<T> {
+public abstract class AbstractDdfService<T, ID> {
 
     protected T save(T t){
         return null;
     }
 
-    protected T findById(Long id) {
+    protected T findById(ID id) {
         return null;
     }
 
