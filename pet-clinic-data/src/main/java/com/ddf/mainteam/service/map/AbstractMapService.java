2c219246ef62cf9b538da8d148942b6a479731d8
InnocentTIALO <tialoinnocent@gmail.com>
2020-05-25 23:13:25 +0100

    Update Map Impl to manage setting of ID property. closes #18

--- pet-clinic-data/src/main/java/com/ddf/mainteam/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/ddf/mainteam/service/map/AbstractMapService.java
@@ -4,22 +4,31 @@ import com.ddf.mainteam.model.BaseEntity;
 import com.ddf.mainteam.service.AbstractDdfService;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * Created by InnocentTIALO on 5/23/2020.
  */
-public abstract class AbstractMapService<T extends BaseEntity, ID> implements AbstractDdfService<T, ID>{
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> implements AbstractDdfService<T, ID>{
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
-    T save(ID id, T t) {
+    T save(Long id, T t) {
         return map.put(id, t);
+    }
+
+
+    @Override
+    public T save(T t) {
+        t.setId(Optional.ofNullable(t.getId()).orElse(getNextId()));
+        return save(getNextId(), t);
     }
 
     @Override
@@ -47,5 +56,9 @@ public abstract class AbstractMapService<T extends BaseEntity, ID> implements Ab
     @Override
     public void deleteById(ID id) {
         map.remove(id);
+    }
+
+    public Long getNextId() {
+        return Collections.max(map.keySet()) + 1;
     }
 }
--- pet-clinic-data/src/main/java/com/ddf/mainteam/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/ddf/mainteam/service/map/OwnerServiceMap.java
@@ -9,10 +9,6 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
-    @Override
-    public Owner save(Owner owner) {
-        return super.save(owner.getId(), owner);
-    }
 
     @Override
     public Owner findByLastName(String lastName) {
